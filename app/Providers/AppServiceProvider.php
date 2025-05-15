580b3efc7e63264c56c4b9c6af9804ccb8804c69
mdkChaos <mdkchaos1@gmail.com>
2025-05-15 14:32:04 +0200

    Reordered migrations: roles before users to avoid foreign key constraint error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::enableForeignKeyConstraints();
+
         Paginator::useBootstrapFive();
 
         Blade::if('role', function ($roleName) {
